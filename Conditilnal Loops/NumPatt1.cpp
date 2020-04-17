4634085d0dfda87bffa7d1bad3d611d631b0f012
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-04-17 11:16:48 +0530

    Create NumPatt1.cpp

---
+++ Conditilnal Loops/NumPatt1.cpp
@@ -0,0 +1,46 @@
+Number Pattern 1
+Print the following pattern
+Pattern for N = 4
+1
+23
+345
+4567
+Input Format :
+N (Total no. of rows)
+Output Format :
+Pattern in N lines
+Sample Input 1 :
+3
+Sample Output 1 :
+1
+23
+345
+/************************************************** SOLUTION *****************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+    int N;
+    cin>>N;
+    int i=1;
+    int val=1;
+    while(i<=N){
+        int j=1;
+        val=i;
+        while(j<=i){
+            cout<<val;
+            j=j+1;
+            val=val+1;
+        }
+        cout<<endl;
+        i=i+1;  
+    }
+}
+
+
