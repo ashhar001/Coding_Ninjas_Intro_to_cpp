e4ed8c0e54ad4e1bc3da9f55bdc47e3fe7d09aa7
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-04-17 16:30:54 +0530

    Create Alpha_P.cpp

---
+++ Pattern 1/Alpha_P.cpp
@@ -0,0 +1,51 @@
+Code : Alpha Pattern
+Send Feedback
+Print the following pattern for the given N number of rows.
+Pattern for N = 3
+ A
+ BB
+ CCC
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 26
+Sample Input 1:
+7
+Sample Output 1:
+A
+BB
+CCC
+DDDD
+EEEEE
+FFFFFF
+GGGGGGG
+Sample Input 2:
+6
+Sample Output 2:
+A
+BB
+CCC
+DDDD
+EEEEE
+FFFFFF
+
+
+/***************************************************** SOLUTION ***********************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+    cin>>n;
+    int i=1;
+    while(i <= n){
+        for(int j = 1; j <= i; j++ ){
+            char ch = 'A'+i-1;
+            cout << ch;
+        }
+        cout<<endl;
+        i++;
+    }   
+}
