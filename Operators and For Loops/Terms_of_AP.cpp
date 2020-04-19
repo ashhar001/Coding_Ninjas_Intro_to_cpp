262b58992a4591aa7469752cda5e000bdfc5161c
Ashar Ansari <52534529+ashhar001@users.noreply.github.com>
2020-04-19 11:34:49 +0530

    Create Terms_of_AP.cpp

---
+++ Operators and For Loops/Terms_of_AP.cpp
@@ -0,0 +1,22 @@
+Terms of AP
+Write a program to print first x terms of the series 3N + 2 which are not multiples of 4.
+Input format :
+Integer x
+Output format :
+Terms of series (separated by space)
+Constraints :
+1 <= N <= 1,000
+Sample Input 1 :
+10
+Sample Output 1 :
+5 11 14 17 23 26 29 35 38 41
+Sample Input 2 :
+4
+Sample Output 2 :
+5 11 14 17
+
+
+/*************************************************** SOLUTION *************************************************************************/
+
+
+/*************************************************** SOLUTION *************************************************************************/
