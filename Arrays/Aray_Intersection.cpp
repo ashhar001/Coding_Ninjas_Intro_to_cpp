9d232c94f23d829aa0e63e2d4d66b6bdda92ead3
Mr Aman yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2022-04-24 12:51:55 +0530

    Update Aray_Intersection.cpp
    There is space on line 46 instead of endl

--- Arrays/Aray_Intersection.cpp
+++ Arrays/Aray_Intersection.cpp
@@ -43,7 +43,7 @@ void intersection(int input1[], int input2[], int size1, int size2) {
     for(int i = 0; i < size1; i++){
         for(int j = 0; j < size2; j++){
             if(input1[i] == input2[j] && (input2[j] !=input3[j])){
-                cout << input2[j] << endl;
+                cout << input2[j] << " " ;
                 input3[j] = input2[j];
                 break;
             }
