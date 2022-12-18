8719519b0442b0687728f96e7a4cfa0c2992c144
twaritagrawal <103057344+twaritagrawal@users.noreply.github.com>
2022-12-18 16:39:49 +0530

    Update RemoveConsecutiveDuplicates.cpp

--- Character Arrays and 2D Arrays/RemoveConsecutiveDuplicates.cpp
+++ Character Arrays and 2D Arrays/RemoveConsecutiveDuplicates.cpp
@@ -10,18 +10,20 @@ abcba
 
 
 void removeConsecutiveDuplicates(char input[]) { 
-    int nextIndex = 1;
-    char lastChar = input[0];
-    for(int i = 0; input[i] != '\0';) { 
-        if(input[i] == lastChar) { 
-            i++; 
-        } 
-        else {
-            input[nextIndex] = input[i];
-            nextIndex++; 
-            lastChar = input[i];
-            i++;
+        int k=0;
+    int l=strlen(input);
+    int j;
+    for(int i=0;i<l;i++){
+        for(j=i+1;j<l;j++){
+            if(input[i]!=input[j]){
+                j=j-1;
+                break;
+                
+            }
         }
-    }
-    input[nextIndex] = '\0';
+            input[k]=input[i];
+            k++;
+            i=j;
+        }
+    input[k]='\0';
 }
